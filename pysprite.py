d8e7d31319d442d2577f4720bbc31fcee6ff3d68
Ingo Hinterding <ingo@hinterding.de>
2020-05-25 19:30:58 +0200

    first working exporter

--- pysprite.py
+++ pysprite.py
@@ -6,9 +6,6 @@
 #   converts a bitmap with a spritesheet of (24*X)*(21*x) pixels
 #   into a sprite data binary
 #
-#   notes:
-#   I found the conversion to be quite tricky since I retrieve the pixel information
-#   from the image itself 
 #
 #############################################
 
@@ -23,26 +20,25 @@ def get_sprites(image_path,x,y):
     i.x, i.y = i.size
     i.col = i.x / x
     i.row = i.y / y
-    i.total = i.row * i.col
+    i.total = int(i.row * i.col)
     i.sprite_width = x
     i.sprite_height = y
-
-    print("spritesheet is: ",i.row, " rows by ", i.col, " cols.")
     return i
 
 
-def show_sprite(sprites,number):
+
+def convert_sprite(sprites,number):
     sprite_length = sprites.sprite_width * sprites.sprite_height
     row = int(math.ceil(number / sprites.col)) -1 
     row_offset = row * (sprite_length * sprites.col) 
     col = number - row*sprites.col -1
     col_offset = col * sprites.sprite_width
-
     start = int(row_offset + col_offset)
     end = int(start + sprite_length)
 
     line = 0
     pixel_count = 0
+    bit_array = []
     
     for i in range(start,end):
         pixel_count = pixel_count + 1
@@ -52,20 +48,63 @@ def show_sprite(sprites,number):
 
         if pixel == 0:
             print(" ", end='')
+            bit_array.append(0)
         else:
             print("#", end='')
+            bit_array.append(1)
 
         if pixel_count == sprites.sprite_width:
             print()
             pixel_count = 0
             line = line + sprites.x -sprites.sprite_width
-    print()
-        
-
-sprites = get_sprites("sprites-001.png", 24, 21)
-#show_sprites(sprites)
-show_sprite(sprites,13)
+    return bit_array
 
 
+def bits_to_bytes(data):
 
+    byte_counter = 0
+    byte = ""
+    bytes_array = []
+
+    for pixel in data:
+        byte = byte + str(pixel)
+
+        if byte_counter == 7:
+            byte_counter = 0
+            hex_number = int(byte,2)
+            bytes_array.append(hex_number)
+            byte = ""
+        else:
+            byte_counter = byte_counter + 1
+    bytes_array.append(1)
+    return bytes_array
+
+
+
+def spritesheet_to_spd(filename, image):
+    all_sprites = []
+
+    for i in range(1,image.total+1):
+        sprite_data = convert_sprite(image,i)
+        sprite_bytes = bits_to_bytes(sprite_data)
+        all_sprites = all_sprites + sprite_bytes
+    
+    write_file(filename,all_sprites)
+
+
+def write_file(filename, data):
+    if not filename:
+        filename = "test"
+
+    newFile = open(filename+".spd", "wb")
+    file_array = [11,8,6]
+    file_array = file_array + data 
+    newFileByteArray = bytearray(file_array)
+    newFile.write(newFileByteArray)
+
+#-----------------------------------------------------------------
+
+
+image = get_sprites("sprites-001.png", 24, 21)
+spritesheet_to_spd("hallo",image)
 
